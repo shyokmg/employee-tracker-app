c9a4eee3842f5b561494a4e48f31bede5e968a2d
Shoyo Kumagai <shyokmg.au@gmail.com>
2023-11-11 22:31:30 +1100

    Structure - restructered the whole process with CLI and handling queries using classes

--- index.js
+++ index.js
@@ -1,17 +1,6 @@
-const express = require('express');
-// const sequelize = require('./config/connection.js');
+// const express = require('express');
 
 const CLI = require('./lib/cli.js');
 const cli = new CLI();
-
-const app = express();
-const PORT = process.env.PORT || 3001;
-
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-
-// sequelize.sync().then(() => {
-//     app.listen(PORT, () => console.log('Now Listening'))
-// });
 
 cli.run();
